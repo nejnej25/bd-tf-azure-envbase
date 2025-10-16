ed7a0f60d82e774d2c9b1afed24fc88e653561fe
nejnej25 <pajulbra@karangalan.com>
2025-10-17 02:20:20 +0800

    added rtable subnet mapping

--- main.tf
+++ main.tf
@@ -29,13 +29,8 @@ module "virtual_networks" {
   vnet_ranges           = each.value.vnet_ranges
   subnets               = each.value.subnets
   subnet_nsg_mapping    = { for k, v in each.value.subnets : k => module.network_security_groups[k].id }
-  #tsubnet_rtable_mapping = { for k, v in each.value.subnets : k => module.route_tables["default"].id }
+  subnet_rtable_mapping = { for k, v in each.value.subnets : k => module.route_tables["default"].id }
   tags                  = local.tags
-
-  depends_on = [
-    module.network_security_groups,
-    module.route_tables
-  ]
 }
 
 module "network_security_groups" {
